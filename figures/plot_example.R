8d4c0555976cbb2c0487cb15b75d4d04fa997fa2
Ignacio Girela <girelaignacio@gmail.com>
2024-09-17 12:15:58 -0300

    add figure folder

---
+++ figures/plot_example.R
@@ -0,0 +1,12 @@
+file_path <- paste(getwd(),"/results/EXPERIMENT1_reps98_MPI_dataset1", sep="")
+load_results <- readRDS(file_path)
+
+plot.data <- do.call("rbind",results)
+#plot.data$country <- as.factor(stringr::str_extract(rownames(plot.data), "[^.]+"))
+#plot.data$year <- as.factor(stringr::str_extract(rownames(plot.data), "\\b\\w+$"))
+plot.data <- reshape2::melt(plot.data)
+plot.data$variable <- as.factor(plot.data$variable)
+
+library(ggplot2)
+ggplot(data = plot.data, aes(x = value, color = variable)) +
+  geom_density() + theme_minimal()
